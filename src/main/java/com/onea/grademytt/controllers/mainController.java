b753e597dde82aecd07d128849cac1696e70acd3
Oliver Enriquez <odec12@gmail.com>
2019-02-01 13:27:50 -0700

    Thymeleaf

--- src/main/java/com/onea/grademytt/controllers/mainController.java
+++ src/main/java/com/onea/grademytt/controllers/mainController.java
@@ -1,12 +1,15 @@
 package com.onea.grademytt.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class mainController {
-    @GetMapping(value = "/home")
-    public String index() {
+
+    @GetMapping(value = "/")
+    public String index(Model model) {
+        model.addAttribute("Hello", "Hello World");
         return "index";
     }
 }
