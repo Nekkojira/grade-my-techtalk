d39f341092f1198d3657382ae6436471b9895802
Oliver Enriquez <odec12@gmail.com>
2019-02-01 09:01:06 -0700

    Spring boot boilerplate

---
+++ src/main/java/com/onea/playground/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.onea.playground.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/")
+	String index(Model model, HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("test");
+		return "index";
+	}
+}
